c123f7afd23bd7e4eafe638532d69c73b44674d7
mohajjalinkhan <mohajjalin01@gmail.com>
2024-11-19 16:49:42 +0530

    added button and created app home page UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_quiz_app/start_screen.dart';
-
 void main() {
   runApp(const MyApp());
 }
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: Container(
+          //created gardiented for background
           decoration: const BoxDecoration(
             gradient: LinearGradient(colors: [
               Color.fromARGB(255, 1, 3, 35),
               Color.fromARGB(255, 41, 3, 63),
             ], begin: Alignment.topLeft, end: Alignment.bottomRight),
           ),
-          child: StartScreen(),
+          child: const StartScreen(),
         ),
       ),
     );
--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -9,11 +9,31 @@ class StartScreen extends StatelessWidget {
       child: Column(
         mainAxisSize: MainAxisSize.min,
         children: [
-          // adding Quiz Imaage
+          // added Quiz Imaage
           Image.asset(
             "assets/images/quiz-logo.png",
             width: 300,
+            // using this for color and opacity
+            color: const Color.fromARGB(255, 253, 252, 255),
           ),
+          // added Sizedbox for padding (Spacing).
+          const SizedBox(height: 80),
+          const Text(
+            "Learn Flutter with Quiz App",
+            style: TextStyle(color: Colors.white, fontSize: 24),
+          ),
+          const SizedBox(height: 30),
+          //added button for navigation to other screen, with icon constructor
+          OutlinedButton.icon(
+            onPressed: () {},
+            style: OutlinedButton.styleFrom(foregroundColor: Colors.white),
+            //added arrow icon in button
+            icon: const Icon(
+              Icons.arrow_right_alt,
+              size: 30,
+            ),
+            label: const Text("Start Quiz"),
+          )
         ],
       ),
     );
